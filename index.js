f337fea96025f173f99610fa4d2e375ba71b958b
BullDogPatch <mancityvsliverpoolfc@outlook.com>
2020-05-25 21:29:03 +0100

    1 file modified

--- index.js
+++ index.js
@@ -3,24 +3,13 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
+function App(){
+  return (
+    <div>
+      <h1>Craig Clayton</h1>
+    </div>
+  )
 }
+
 
 render(<App />, document.getElementById('root'));
